7b6ba6e003d468b97e8442d5c9991152dbe931cb
Jeremy <jeremyeparker@yahoo.com>
2021-11-02 21:26:05 -0400

    extend JpaRepo

--- src/main/java/com/geekylikes/app/repositories/DeveloperRepository.java
+++ src/main/java/com/geekylikes/app/repositories/DeveloperRepository.java
@@ -1,2 +1,7 @@
-package com.geekylikes.app.repositories;public interface DeveloperRepository {
+package com.geekylikes.app.repositories;
+
+import com.geekylikes.app.models.Developer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeveloperRepository extends JpaRepository<Developer,Long> {
 }

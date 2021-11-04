8fffc67a720fdd752d74442d0080c619b0949eb3
Jeremy <jeremyeparker@yahoo.com>
2021-11-03 23:59:27 -0400

    m

--- src/main/java/com/geekylikes/app/models/Developer.java
+++ src/main/java/com/geekylikes/app/models/Developer.java
@@ -11,6 +11,7 @@ public class Developer {
     @GeneratedValue
     private Long id;
     private String name;
+    private String privateName;
     private String email;
     private Integer cohort;
     private String[] languages;
